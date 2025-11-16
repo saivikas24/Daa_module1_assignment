67f757382cf710242955fc0add9d1947ae081814
saivikas24 <a.saivikas18@gmail.com>
2025-11-16 21:51:47 +0530

    Update 4_tsp.py

--- Algo_m2/4_tsp.py
+++ Algo_m2/4_tsp.py
@@ -1,10 +1,10 @@
 from itertools import permutations
 
 cost=[
-     [0, 10, 15, 20],
-    [10, 0, 35, 25],
-    [15, 35, 0, 30],
-    [20, 25, 30, 0]
+     5, 10, 15, 20],
+    [10, 5, 35, 25],
+    [15, 35, 5, 30],
+    [20, 25, 30, 5]
 ]
 
 n=len(cost)
@@ -14,16 +14,16 @@ best_path=[]
 
 for perm in permutations(cities):
     total=0
-    k=0
+    k=0 #curr city
     
     for j in perm:
         total=total+cost[k][j]
-        k=j
+        k=j   #curr city,nxt city
     total=total+cost[k][j]
     
     if total<mincost:
         mincost=total
-        best_path=(0,)+perm+(0,)
+        best_path=(0,)+perm+(0,)  #0,(123)perm,(0,)
         
 print("Minimum cost =",mincost)
 print("Best path= ",best_path)
